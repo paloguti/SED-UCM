2630eb9fee8aad5e83466ff42c9a60dc3c9ede09
Paloma Gutiérrez <paloguti@ucm.es>
2019-02-13 20:00:54 +0100

    Apartado 1 y 2 funcionando

---
+++ practica1/Debug/button.d
@@ -0,0 +1,13 @@
+button.d button.o: ../button.c \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44blib.h \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/option.h \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/def.h
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44blib.h:
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/option.h:
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/def.h:
---
+++ practica1/Debug/led.d
@@ -0,0 +1,10 @@
+led.d led.o: ../led.c \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/option.h \
+ C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44blib.h
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/option.h:
+
+C:\hlocal\EclipseARMwspace\proyecto100\commonEclipse/44blib.h:
