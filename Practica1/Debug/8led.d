d74163569db11bcd1140e8cc2693cdf6ef4a36f9
Rocío Santos <rocio.santosbuitrago@telefonica.com>
2019-02-19 16:49:41 +0100

    Pr1 completa

---
+++ Practica1/Debug/8led.d
@@ -0,0 +1,10 @@
+8led.d 8led.o: ../8led.c \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/option.h \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44blib.h
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/option.h:
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44blib.h:
---
+++ Practica1/Debug/main.d
@@ -0,0 +1,10 @@
+main.d main.o: ../main.c \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44blib.h \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44b.h \
+ C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/option.h
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44blib.h:
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/44b.h:
+
+C:\hlocal\EclipseARMwspace\Practica1\commonEclipse/option.h:
